af74f750b98b7d1017a0dc691683f6125186f473
Zain Bin Samin <32740828+zain0192@users.noreply.github.com>
2026-03-31 13:47:44 +0500

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let left = 0
+        let right = nums.length - 1;
+
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2)
+
+            if (nums[mid] === target) {
+                return mid
+            } else if (nums[mid] > target) {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+
+        return -1
+    }
+}
