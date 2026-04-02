7b4a20aeddd4197878d2fdb4ce8eafc003adf4e8
Zain Bin Samin <32740828+zain0192@users.noreply.github.com>
2026-04-02 13:18:34 +0500

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let output = [];
+
+        for(let str of strs) {
+            let sorted = str.split("").sort().join("")
+            if(output[sorted]) {
+                output[sorted].push(str)
+            } else {
+                output[sorted] = [str]
+            }
+        }
+        console.log(output)
+        let res = []
+        for (let key in output) {
+            res.push(output[key])
+        }
+        return res
+    }
+}
