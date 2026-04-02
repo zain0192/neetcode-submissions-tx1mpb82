0a3cb813ad6f02dbedd0d182ded92c57619b9800
Zain Bin Samin <32740828+zain0192@users.noreply.github.com>
2026-04-02 13:51:24 +0500

    Add: top-k-elements-in-list - submission-13

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-13.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const counts = {}
+        let highest = 0
+
+        for(let num of nums) {
+            counts[num] = (counts[num] || 0) + 1
+            if (counts[num] > highest) {
+                highest = counts[num]
+            }
+        }
+        const output = Object.entries(counts).map(([num, freq]) => [
+            freq,
+            parseInt(num)
+        ])
+        output.sort((a,b) => b[0] - a[0])
+        return output.slice(0, k).map((pair) => pair[1])
+    }
+}
