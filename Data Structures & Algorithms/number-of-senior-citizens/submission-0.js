2a1b7ba924f20095946539b9c50d4973a2a0582a
Zain Bin Samin <32740828+zain0192@users.noreply.github.com>
2026-04-08 17:48:13 +0500

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string[]} details
+     * @return {number}
+     */
+    countSeniors(details) {
+        let count = 0
+        for (let i = 0; i < details.length; i++) {
+            let age = parseInt(details[i].slice(11,13))
+            if (age > 60) {
+                count++
+            }
+        } 
+
+        return count
+    }
+}
